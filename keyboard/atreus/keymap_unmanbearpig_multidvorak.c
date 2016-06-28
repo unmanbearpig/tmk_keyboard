236e5ad52b548b936c2d85250aaaad7578f37028
unmanbearpig <unmanbearpig@gmail.com>
2016-06-28 14:04:09 +0300

    Make left-hand modifier keys consistent

--- keyboard/atreus/keymap_unmanbearpig_multidvorak.c
+++ keyboard/atreus/keymap_unmanbearpig_multidvorak.c
@@ -33,13 +33,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
          KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11, \
          KC_NO, KC__VOLUP, KC_NO, KC_NO, KC_FN3, KC_FN5, KC_F1, KC_F2, KC_F3, KC_F12, \
-         KC_NO, KC__VOLDOWN, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE), \
+         KC_NO, KC__VOLDOWN, KC_BSPC, KC_LGUI, KC_LSFT, KC_LCTL, KC_LALT, KC_SPC, KC_FN2, KC_PSCREEN, KC_SLCK, KC_PAUSE), \
 
   /* 3: hard dvorak */
   KEYMAP(KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, \
          KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, \
          KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, \
-         KC_ESC, KC_TAB, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT,     \
+         KC_ESC, KC_TAB, KC_BSPC, KC_LGUI, KC_LSFT, KC_LCTL, KC_LALT,     \
          KC_SPC, KC_FN6, KC_MINS, KC_QUOT, KC_ENT),                     \
 
   /* 4: hard dvorak fn */
@@ -49,14 +49,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
            KC_PGDN, KC_4, KC_5, KC_6, SHIFT(KC_RBRC), \
          KC_MINS, KC_EQUAL, SHIFT(KC_9), SHIFT(KC_0), SHIFT(KC_7),      \
            KC_GRAVE, KC_1, KC_2, KC_3, KC_BSLS,                       \
-         KC_FN7, SHIFT(KC_INS), KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, \
+         KC_FN7, SHIFT(KC_INS), KC_BSPC, KC_LGUI, KC_LSFT, KC_LCTL, KC_LALT, \
            KC_SPC, KC_FN6, KC_DOT, KC_0, KC_EQUAL), \
 
   /* 5: hard dvorak L2 */
   KEYMAP(KC_INS, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_UP, KC_F7, KC_F8, KC_F9, KC_F10, \
          KC_DEL, KC_LEFT, KC_DOWN, KC_RIGHT, KC_PGDN, KC_DOWN, KC_F4, KC_F5, KC_F6, KC_F11, \
          KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_F1, KC_F2, KC_F3, KC_F12, \
-         KC_NO, KC_NO, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC, KC_FN8, KC_NO, KC_NO, KC_FN10)
+         KC_NO, KC_NO, KC_BSPC, KC_LGUI, KC_LSFT, KC_LCTL, KC_LALT, KC_SPC, KC_FN8, KC_NO, KC_NO, KC_FN10)
 
 };
 
